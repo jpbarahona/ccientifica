84b8398c2a5258fe1405f0d2eb80f8a2ae3befec
Jupaba <juanpablo.barahonaa@gmail.com>
2015-11-22 16:28:06 -0300

    Add static folder

---
+++ webapp/static/js/common.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+		$(".menuFooter").html("<div class='wrapper row4'>"+
+			"<footer id='footer' class='clear'>"+
+				"<h2 class='font-x2 uppercase' align='center'>Equipo de Trabajo</h2><hr>"+
+					"<div id='owl-demo'>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>JuanPablo Barahona</h6>"+
+							"<address class='btmspace-15'>"+
+							"Estudiante Univerdad<br>"+
+							"Ing. Computación e Informática<br>"+
+							"U. Andrés Bello<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> juanpablo.barahonaa@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp0.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>J. Antonio Lefimil</h6>"+
+							"<address class='btmspace-15'>"+
+							"Estudiante Universidad<br>"+
+							"Ing. Computación Científica<br>"+
+							"Andrés Bello<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> antonio.lefimil@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp2.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>Gonzalo Cifuentes</h6>"+
+							"<address class='btmspace-15'>"+
+							"Estudiante Univerdad<br>"+
+							"Ing. Computación e Informática<br>"+
+							"U. Andrés Bello<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> gonzalo.cifuentes@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp3.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>Alfredo González</h6>"+
+							"<address class='btmspace-15'>"+
+							"Estudiante Universidad<br>"+
+							"Ing. Computación Científica<br>"+
+							"Andrés Bello<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> alpihola@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp4.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>Gustavo Gatica</h6>"+
+							"<address class='btmspace-15'>"+
+								"Universidad Andrés Bello<br>"+
+								"Profesor guía<br>"+
+								"Curso Computación Científica<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> ggatica@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+						"<div class='item'>"+
+							"<img src='webapp/static/images/pp5.jpg' alt='Owl Image'>"+
+							"<h6 class='title'>Matías Greco</h6>"+
+							"<address class='btmspace-15'>"+
+							"Estudiante Univerdad<br>"+
+							"Ing. Computación e Informática<br>"+
+							"U. Andrés Bello<br>"+
+							"</address>"+
+							"<ul class='nospace'>"+
+								"<li><span class='fa fa-envelope-o'></span> m.grecochandia@gmail.com</li>"+
+							"</ul>"+
+						"</div>"+
+					"</div>"+
+			"</footer>"+
+		"</div>"+
+		"<div class='wrapper row5'>"+
+			"<div id='copyright' class='clear'>"+
+				"<p class='fl_left'>Copyright &copy; 2015 - All Rights Reserved - <a href='#'>Domain Name</a></p>"+
+				"<p class='fl_right'>Template by <a target='_blank' href='http://www.os-templates.com/' title='Free Website Templates'>OS Templates</a></p>"+
+			"</div>"+
+		"</div>"+
+		"<a id='backtotop' href='#top'><i class='fa fa-chevron-up'></i></a>");
+});
