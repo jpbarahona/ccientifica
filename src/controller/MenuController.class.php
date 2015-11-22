4b2af36a4ca9ec0e6c891958d927c60f8f33520e
Jupaba <juanpablo.barahonaa@gmail.com>
2015-11-22 16:37:38 -0300

    Actualizado menu controller

--- src/controller/MenuController.class.php
+++ src/controller/MenuController.class.php
@@ -7,21 +7,52 @@
 		public function index(){
 			return 'index';
 		}
-		public function algBrent(){
-			return 'example';
+		public function Biseccion(){
+			return 'algoritmos/raices-de-ecuaciones/biseccion';
 		}
-		public function repost(){
-			return 'repost';
+		public function Brent(){
+			return 'algoritmos/raices-de-ecuaciones/brent';
 		}
-			public function calculator(){
-			return 'sidebar-left';
+		public function Secante(){
+			return 'algoritmos/raices-de-ecuaciones/secante';
 		}
-		public function SPLine(){
-			return 'spline';
+		public function Punto(){
+			return 'algoritmos/raices-de-ecuaciones/puntofijo';
 		}
-
-		public function lagrange(){
-			return 'lagrange';
+		public function Falsa(){
+			return 'algoritmos/raices-de-ecuaciones/reglafalsa';
+		}
+ 		/*Curvas*/
+		public function Polinomial(){
+			return 'algoritmos/ajustes-de-curvas/polinomial';
+		}
+		public function Lineal(){
+			return 'algoritmos/ajustes-de-curvas/lineal';
+		}
+		public function Newton(){
+			return 'algoritmos/ajustes-de-curvas/newton';
+		}
+		public function Discretos(){
+			return 'algoritmos/ajustes-de-curvas/discretos';
+		}
+		public function Continuos(){
+			return 'algoritmos/ajustes-de-curvas/continuos';
+		}
+		public function Lagrange(){
+			return 'algoritmos/ajustes-de-curvas/lagrange';
+		}
+		public function Spline(){
+			return 'algoritmos/ajustes-de-curvas/spline';
+		}			
+		/*Otros*/
+		public function Euler(){
+			return 'algoritmos/optimizacion/euler';
+		}
+		public function Runge(){
+			return 'algoritmos/ecuacion-diferencial/runge';
+		}
+		public function Heun(){
+			return 'algoritmos/ecuacion-diferencial/heun';
 		}
 	}
 ?>
